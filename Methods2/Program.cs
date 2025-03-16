37b556a3cce9d5d4ed805ca83ff7b87f0c158fd4
Gu1yana <gulyanamk-bp110@code.edu.az>
2025-03-16 20:50:11 +0400

    Minimumutap

---
+++ Methods2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Methods2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 23, 654, 47, 238, -23, 532, 1, 980 };
+            Console.WriteLine(Min(arr));
+        }
+        static int Min(int[] arr)
+        {
+
+            int min = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (min > arr[i])
+                    min = arr[i];
+            }
+            return min;
+        }
+    }
+}
